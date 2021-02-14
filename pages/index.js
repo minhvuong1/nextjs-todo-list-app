d4c5cfe29f0fe387bb10e313dd28f2cde86810cc
Minh Vuong <minhvuong@live.com>
2021-02-14 21:26:57 +1100

    title change

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>To Do App</title>
+        <title>To Do List App</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
       </Head>
